10f02d9937e7af98c3a4f58506c1997b19648e91
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-07 18:29:03 +0500

    Add discount calculation functionality to question_2.py

--- question_2.py
+++ question_2.py
@@ -1,2 +1,10 @@
 # Price After Discount
 
+price = int(input("Enter the price: "))
+discount = int(input("Enter the discount: "))
+
+def discount_func(price, discount):
+    discount = price - (price * discount / 100)
+    return discount
+
+print(discount_func(price, discount))
