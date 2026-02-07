b4f0d559b68792c2d080e42484722f1ab3f23e57
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-07 18:20:45 +0500

    Add login functionality with user authentication

--- question_1.py
+++ question_1.py
@@ -1,1 +1,15 @@
 # Login Check
+
+users = {"admin": "1234", "user1": "abcd", "ali": "pass123"}
+
+def login(users):
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    if username in users:
+        if password in users.values():
+            print("Login Successfull!")
+    else:
+        print("Invalid Credentials")
+
+
+login(users)
