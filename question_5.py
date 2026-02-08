3c5c9084ca2682ee1ef05e91b4a018a3864d1cfe
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-08 11:12:50 +0500

    Add email validation functionality in question_5.py to check for valid email format

--- question_5.py
+++ question_5.py
@@ -1,1 +1,9 @@
 # Email Validator
+
+email = input("Enter your email: ")
+
+def email_validator(email):
+    check = "Valid Email" if "@" in email and "." in email else "Invalid Email"
+    return check
+
+print(email_validator(email))
