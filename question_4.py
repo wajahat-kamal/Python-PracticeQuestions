dbc7afd877abe6d810f95d28c2e9741dee4a8991
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-08 10:35:29 +0500

    Fix return statement in stock_qty function in question_4.py to ensure correct stock status output

--- question_4.py
+++ question_4.py
@@ -3,10 +3,8 @@
 stock = {"apple": 5, "banana": 0, "orange": 10, "pineaple": 0}
 
 
-def stock_qty(stock):
-    for product, qty in stock.items():
-        if qty == 0:
-            return f"{product} is out of stock"
-        else:
-            f"{product} is in stock"
-print(stock_qty(stock))
+for product, qty in stock.items():
+    if qty == 0:
+        print(f"{product} is out of stock")
+    else:
+        print(f"{product} is in stock")
