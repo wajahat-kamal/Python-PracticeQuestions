32c04fb5d5b5210bf720d450087fedd7ff9f70f1
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-08 11:40:24 +0500

    Add password length validation in question_8.py to ensure passwords meet minimum length requirement

--- question_8.py
+++ question_8.py
@@ -1,2 +1,11 @@
 # Password Length Check
 
+password = input("Enter your password: ")
+
+def check_password(password):
+    if len(password) < 8:
+        return "Password is too short"
+    else:
+        return "Strong Password"
+
+print(check_password(password))
