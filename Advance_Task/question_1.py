e7eac4b294b76089161d4507b46fbd633473d199
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-08 12:43:30 +0500

    Refactor invoice generation logic in question_1.py by encapsulating product input and total calculation within a function, enhancing code organization and readability.

--- Advance_Task/question_1.py
+++ Advance_Task/question_1.py
@@ -1,25 +1,29 @@
+# Mini Invoice Generator
+
 products = []
 num = int(input("How many Products? "))
 
-for i in range(num):
-    name = input("Enter product name: ")
-    price = int(input("Enter product price: "))
-    qty = int(input("Enter product quantity: "))
-    products.append({"name": name, "price": price, "qty": qty})
+def invoice_generator(products, num):
+    for i in range(num):
+        name = input("Enter product name: ")
+        price = int(input("Enter product price: "))
+        qty = int(input("Enter product quantity: "))
+        products.append({"name": name, "price": price, "qty": qty})
+
+    total = 0
+    for product in products:
+        subTotal = product["price"] * product["qty"]
+        total += subTotal
+        print(f"{product['name']} X {product['qty']} = {subTotal}")
+
+    tax = round(total * 0.10, 2)
+    discount = round(total * 0.05 if total >= 5000 else 0, 2)
+    final_amount = round(total + tax - discount, 2)
+
+    print(f"{'Total before tax:':<20} {total}")
+    print(f"{'Tax (10%):':<20} {tax}")
+    print(f"{'Discount (5%):':<20} {discount}")
+    print(f"{'Final amount:':<20} {final_amount}")
 
 
-total = 0
-for product in products:
-    subTotal = product["price"] * product["qty"]
-    total += subTotal
-    print(f"{product['name']} X {product['qty']} = {subTotal}")
-
-tax = round(total * 0.10, 2)
-discount = round(total * 0.05 if total >= 5000 else 0, 2)
-final_amount = round(total + tax - discount, 2)
-
-
-print(f"\nTotal before tax: {total}")
-print(f"\nTax (10%): {tax}")
-print(f"\nDiscount (5%): {discount}")
-print(f"Finall amount: {final_amount}")
+invoice_generator(products, num)
