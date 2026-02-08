3d09fda824e5f10c6aa356a13060a418d4effff3
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-08 11:42:53 +0500

    Refactor password validation logic in question_8.py to streamline the check and improve readability

--- question_8.py
+++ question_8.py
@@ -2,10 +2,14 @@
 
 password = input("Enter your password: ")
 
+
 def check_password(password):
-    if len(password) < 8:
-        return "Password is too short"
-    else:
-        return "Strong Password"
+    # if len(password) < 8:
+    #     return "Password is too short"
+    # else:
+    #     return "Strong Password"
+
+    return "Password is too short" if len(password) < 8 else "Strong Password" # in one line
+
 
 print(check_password(password))
