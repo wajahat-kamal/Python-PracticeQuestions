c8688d577aa0086db419efbd39d7991756c49059
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-08 21:08:22 +0500

    Refactor transaction_analyzer function in question_4.py to consolidate credit and debit calculations into single variables, improving efficiency. Added a warning for negative total balance to enhance user awareness.

--- Intermediate_Task/question_4.py
+++ Intermediate_Task/question_4.py
@@ -4,14 +4,17 @@ transactions = [2000, -500, -300, 1500, -1000]
 
 
 def transaction_analyzer(transactions):
-    credit = []
-    debit = []
+    credit = 0
+    debit = 0
     total_balance = sum(transactions)
     for transaction in transactions:
         if transaction > 0:
-            credit.append(transaction)
+            credit += transaction
         else:
-            debit.append(transaction)
+            debit += abs(transaction)
+    
+    if total_balance < 0:
+        "Warning: Your balance is negative!"
 
     return f"Total Credit: {credit} \nTotal Debit: {debit} \nTotal Balance: {total_balance}"
 
