9be7b7e2f85d71bfdc35fa9e9be63cfc30af0889
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-08 11:20:21 +0500

    Implement email format validation in question_5.py to ensure user input meets standard criteria

---
+++ question_6.py
@@ -0,0 +1,12 @@
+# Total Bill (Sum of Items)
+
+prices = [250, 100, 340, 120]
+
+def sum(prices):
+    total = 0
+    for price in prices:
+        sum += price
+
+    return f"Total bill is {total}"
+
+print(sum(prices))
