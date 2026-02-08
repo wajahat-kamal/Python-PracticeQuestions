09f3661952a3c8e1b4eefa7776768d68ea53e251
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-08 18:22:10 +0500

    Refactor variable names in question_3.py for clarity, changing 'list' to 'marks' and correcting percentage comparison from string to integer, enhancing code readability and functionality.

--- Intermediate_Task/question_3.py
+++ Intermediate_Task/question_3.py
@@ -1,21 +1,21 @@
 # Student Result System
 
-list = [78, 85, 62, 90, 55]
+marks = [78, 85, 62, 90, 55]
 
 
-def result_system(list):
+def result_system(marks):
     total_marks = 0
-    for n in list:
+    for n in marks:
         total_marks += n
-    
-    percentage = (total_marks / 500) * 100
+
+    percentage = (total_marks / ()) * 100
     grade = ""
 
     if percentage >= 80:
         grade = "A"
     elif percentage >= 60:
         grade = "B"
-    elif percentage >= "40":
+    elif percentage >= 40:
         grade = "C"
     else:
         grade = "Fail"
@@ -23,5 +23,4 @@ def result_system(list):
     return f"Total Marks: {total_marks} \nPercentage: {percentage} \nGrade: {grade}"
 
 
-
-print(result_system(list))
+print(result_system(marks))
