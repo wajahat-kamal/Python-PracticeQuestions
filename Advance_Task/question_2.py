54d1417acfbfc21857a6fb50205bc06f2250d3c0
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-08 18:02:06 +0500

    Fix attendance percentage calculation in attendance_tracker function of question_2.py to accurately reflect present and absent counts, improving output clarity.

--- Advance_Task/question_2.py
+++ Advance_Task/question_2.py
@@ -6,17 +6,14 @@ attendance = ["present", "absent", "present", "present", "absent"]
 def attendance_tracker(attendance):
     total = len(attendance)
     present = 0
-    absent = 0
 
     for i in attendance:
         if i == "present":
             present += 1
-        else:
-            absent += 1
 
     percentage = (present / total) * 100
 
-    return f"Total Students are: {total} \n Present {present} \n Absent {absent} \n Attendance Percentage: {percentage}%"
+    return f"Total Students are: {total} \n Present {present} \n Absent {total - present} \n Attendance Percentage: {percentage}%"
 
 
 print(attendance_tracker(attendance))
