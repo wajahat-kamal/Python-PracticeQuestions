79d5e8189b2309776c0a58750069026d9acb939c
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-08 11:37:13 +0500

    Refactor uppercase function in question_7.py to use list comprehension for improved readability and efficiency

--- question_7.py
+++ question_7.py
@@ -4,11 +4,13 @@ names = ["ali", "sara", "umer", "wajahat", "kamal"]
 
 
 def uppercase(names):
-    upperNames = []
-    for name in names:
-        upperNames.append(name.upper())
+    # upperNames = []
+    # for name in names:
+    #     upperNames.append(name.upper())
 
-    return upperNames
+    # return upperNames
+
+    return [name.upper() for name in names] # in one line
 
 
 print(uppercase(names))
