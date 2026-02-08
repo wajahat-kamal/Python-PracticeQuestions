75f1d7099213bcabc7f98830b8ccd2ba87e8cb4d
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-08 11:04:44 +0500

    Refactor stock status output in question_4.py to simplify logic and improve readability

--- question_4.py
+++ question_4.py
@@ -4,7 +4,5 @@ stock = {"apple": 5, "banana": 0, "orange": 10, "pineaple": 0}
 
 
 for product, qty in stock.items():
-    if qty == 0:
-        print(f"{product} is out of stock")
-    else:
-        print(f"{product} is in stock")
+    status = "is out of stock" if qty == 0 else "is in stock"
+    print(f"{product} {status}")
