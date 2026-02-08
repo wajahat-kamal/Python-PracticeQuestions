cf974a7a337617575bbe7aac96a4eb0499c77805
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-08 11:47:26 +0500

    Add user existence check in question_9.py to verify if a name is in the predefined user list

--- question_9.py
+++ question_9.py
@@ -1,1 +1,13 @@
 # Find User
+
+users = ["ali", "sara", "umer", "wajahat", "kamal"]
+
+def check_user(users):
+    user = input("Enter your name: ")
+
+    if user in users:
+        return "User Exists"
+    else:
+        return "User not found"
+
+print(check_user(users))
