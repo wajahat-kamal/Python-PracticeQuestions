e615972a427cd1c089baff298d967de0127ec543
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-11 17:08:45 +0500

    Add atm_withdrawal_system function in question_6.py to prepare for ATM withdrawal functionality, maintaining user input for balance and amount.

--- Intermediate_Task/question_6.py
+++ Intermediate_Task/question_6.py
@@ -4,3 +4,7 @@ balance = int(input("Enter your bank balance: "))
 amount = int(input("Enter your amount: "))
 
 
+def atm_withdrawal_system(balance, amount):
+    pass
+
+print(atm_withdrawal_system(balance, amount))
