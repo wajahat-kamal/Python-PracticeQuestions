298033444282540a259888ccc58746410344bc50
wajahat-kamal <wajahatkamal3.0@gmail.com>
2026-02-11 17:15:09 +0500

    Refactor atm_withdrawal_system function in question_6.py to improve balance validation by including transaction fees in the insufficient balance check, and update return format to a dictionary for clearer user feedback.

--- Intermediate_Task/question_6.py
+++ Intermediate_Task/question_6.py
@@ -7,15 +7,16 @@ amount = int(input("Enter your amount: "))
 def atm_withdrawal_system(balance, amount):
     if amount <= 0:
         return "Invalid Amount"
-    if balance < amount:
+
+    transaction_fee = round(amount * 0.02, 2)
+
+    if balance < (amount + transaction_fee):
         return "Insufficient Balance"
 
-
-    transaction_fee = round(amount * 0.02, 2) 
     total_deduction = amount + transaction_fee
-    new_balance = balance - total_deduction
+    new_balance = round(balance - total_deduction, 2)
 
-    return f"Updated Balance: {new_balance} \nTransaction Fee 2%: {transaction_fee}"
+    return {"Updated Balance": {new_balance}, "Transaction Fee 2%": {transaction_fee}}
 
 
 print(atm_withdrawal_system(balance, amount))
